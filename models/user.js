6e1eade14c8cb292a454386c6b1673c354ce3c7c
zsheperdigian <zsheperdigian@gmail.com>
2022-11-03 18:35:43 -0700

    added starter server and model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+// username:
+    // -String
+    // -unique
+    // -required
+    // -trimmed
+
+
+// email:
+    //string
+    // required
+    //quinmique
+    //must match a valid email adress (look up Mongo how to)
+
+//thoughts:
+    //array of id referenceing thought model
+
+
+//friends:
+    //array of id reference user model()
+
+
+//friendCount
+    // retrieve friendarray.length
