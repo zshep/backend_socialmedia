1c67242e9f1e67fbb3cc87cbc6ea7434ca2e7731
zsheperdigian <zsheperdigian@gmail.com>
2022-11-03 22:24:33 -0700

    started to add users models, did not finished

--- models/user.js
+++ models/user.js
@@ -1,23 +1,29 @@
-// username:
-    // -String
-    // -unique
-    // -required
-    // -trimmed
+const mongoose = require('mongoose');
 
-
-// email:
-    //string
-    // required
-    //quinmique
-    //must match a valid email adress (look up Mongo how to)
-
-//thoughts:
-    //array of id referenceing thought model
-
-
-//friends:
-    //array of id reference user model()
-
-
-//friendCount
-    // retrieve friendarray.length
+const userSchema = new mongoose.Schema(
+    {
+        //to do: figure out how to add in trimmed
+        username: {type: String, required: true, unique: true, trimmed: true},
+                    // -trimmed
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //must match a valid email adress (look up Mongo how to)
+    },
+            
+            //thoughts:
+                //array of id referenceing thought model
+            
+            
+            //friends:
+                //array of id reference user model()
+            
+            
+            //friendCount
+                // retrieve friendarray.length
+        
+    }
+    
+    )
+    
